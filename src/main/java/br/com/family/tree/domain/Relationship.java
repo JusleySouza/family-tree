17420b9a55d62152c379fa4a541652386d156203
Jusley Souza <jusleysouza31@gmail.com>
2026-01-19 00:25:32 -0300

    feat(relationship) adding the relationship class

---
+++ src/main/java/br/com/family/tree/domain/Relationship.java
@@ -0,0 +1,26 @@
+package br.com.family.tree.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.RelationshipProperties;
+import org.springframework.data.neo4j.core.schema.TargetNode;
+
+import java.util.UUID;
+
+@RelationshipProperties
+@Getter
+@Setter
+public class Relationship {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @TargetNode
+    private Person destination;
+
+    private TypeKinship typeKinship;
+
+}
