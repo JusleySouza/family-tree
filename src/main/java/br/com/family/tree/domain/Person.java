4f1c9aa3778c111238f96af3e0d957838f295d6d
Jusley Souza <jusleysouza31@gmail.com>
2026-01-18 23:58:04 -0300

    feat(person) adding Person model class

---
+++ src/main/java/br/com/family/tree/domain/Person.java
@@ -0,0 +1,31 @@
+package br.com.family.tree.domain;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Node("Person")
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    private LocalDate dateOfDeath;
+
+}
