4b07779e0c1f5da0b4c3e767a44b2d07da9a95f5
Jusley Souza <jusleysouza31@gmail.com>
2026-01-18 23:35:26 -0300

    feat(family-tree-application) adding main class

---
+++ src/main/java/br/com/family/tree/FamilyTreeAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.family.tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyTreeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyTreeAppApplication.class, args);
+	}
+
+}
