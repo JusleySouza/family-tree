2c274f6525e654513c8414f3e495c64e16bb5824
Jusley Souza <jusleysouza31@gmail.com>
2026-01-22 21:07:36 -0300

    feat(person) Adding methods for date validation and updating the date of death.

--- src/main/java/br/com/family/tree/domain/Person.java
+++ src/main/java/br/com/family/tree/domain/Person.java
@@ -1,8 +1,11 @@
 package br.com.family.tree.domain;
 
+import br.com.family.tree.exception.BusinessRuleException;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
@@ -14,6 +17,8 @@ import java.util.UUID;
 @Node("Person")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
 
     @Id
@@ -27,5 +32,22 @@ public class Person {
     private LocalDate birthDate;
 
     private LocalDate deathDate;
+
+    public void updateDeathDate(LocalDate deathDate) {
+        this.deathDate = deathDate;
+        validateDates();
+    }
+
+    public void validateDates(){
+        if(birthDate == null){
+            throw new BusinessRuleException("Birth date is required.");
+        }
+        if(birthDate.isAfter(LocalDate.now())){
+            throw new BusinessRuleException("Birth date cannot be in the future.");
+        }
+        if(deathDate != null && deathDate.isBefore(birthDate)){
+            throw new BusinessRuleException("Death date cannot be before birth date.");
+        }
+    }
 
 }
