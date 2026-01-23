c4fae56fd90b3a913f603c9709b410839c3e0b8e
Jusley Souza <jusleysouza31@gmail.com>
2026-01-23 17:35:15 -0300

    feat(person-repository) adding queries to find the parents and create a relationship. .

--- src/main/java/br/com/family/tree/repository/PersonRepository.java
+++ src/main/java/br/com/family/tree/repository/PersonRepository.java
@@ -3,6 +3,7 @@ package br.com.family.tree.repository;
 import br.com.family.tree.domain.Person;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.*;
 
@@ -12,6 +13,23 @@ public interface PersonRepository extends Neo4jRepository<Person, UUID> {
         MATCH (p:Person {id: $personId})<-[:FATHER|MOTHER*]-(ancestor)
         RETURN ancestor
             """)
-    List<Person> findAncestors(UUID personId);
+    List<Person> findAncestors(@Param("personId") UUID personId);
+
+    @Query("""
+        MATCH (child:Person {id: $childId})<-[:FATHER|MOTHER]-(parent)
+        RETURN parent
+    """)
+    List<Person> findParents(@Param("childId") UUID childId);
+
+    @Query("""
+        MATCH (from:Person {id: $fromId}),
+              (to:Person {id: $toId})
+        CREATE (from)-[:${type}]->(to)
+    """)
+    void createRelationship(
+            @Param("fromId") UUID fromId,
+            @Param("toId") UUID toId,
+            @Param("type") String type
+    );
 
 }
